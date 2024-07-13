0ba544bb9d3603878db3cbda7680b218e214d360
Vlad <vlad_kopychev@mail.ru>
2024-07-13 17:59:55 +0300

    домашнее задание 007

--- src/homeworks07/App.java
+++ src/homeworks07/App.java
@@ -17,42 +17,53 @@ public class App {
         Person sveta = new Person("Света", -3);
 
 
-        Product bread = new Product("Хлеб", 40);
-        Product milk = new Product("Молоко", 60);
-        Product cake = new Product("Торт", 1000);
-        Product coffee = new Product("Кофе растворимый", 879);
-        Product oil = new Product("Масло", 150);
-        Product iceCream = new Product("Мороженное", 200);
-        Product pasta = new Product("Макароны", 800);
+        Product bread = new Product("Обычный продукт:Хлеб", 40);
+        Product milk = new Product("Обычный продукт:Молоко", 60);
+        Product cake = new Product("Обычный продукт:Торт", 1000);
+        Product coffee = new Product("Обычный продукт:Кофе растворимый", 879);
+        Product oil = new Product("Обычный продукт:Масло", 150);
+        Product iceCream = new Product("Обычный продукт:Мороженное", 200);
+        Product pasta = new Product("Обычный продукт:Макароны", 800);
 
+        DiscountProduct beer = new DiscountProduct("Акционный продукт:Пиво", 250, 38);
+        DiscountProduct pizza = new DiscountProduct("Акционный продукт:Пицца", 700, 101);
+        DiscountProduct waffles = new DiscountProduct("Акционный продукт:Вафли", 100, 49);
+        DiscountProduct hookah = new DiscountProduct("Акционный продукт:Кальян", 1800, 1050);
+        DiscountProduct carhart = new DiscountProduct("Акционный продукт:КАРХАРТ КОРОЛЯ", 99999, 99998);
 
         List<Person> personsCollection = Arrays.asList(pavelAndreevich, annaPetrovna, boris, zhenya, sveta);
         List<Product> productsCollection = Arrays.asList(bread, milk, cake, coffee, oil, iceCream, pasta);
+        List<DiscountProduct> discountProductsCollection = Arrays.asList(beer, pizza, waffles, hookah, carhart);
 
         boolean isEnoughFlag = false;
 
         for (int i = 0; i < productsCollection.size(); i++) {
             if (isEnoughFlag) break;
+            for (int k = 0; k < discountProductsCollection.size(); k++) {
+                if (isEnoughFlag) break;
 
-            for (int j = 0; j < personsCollection.size(); j++) {
-                System.out.println(" Если Вы желаете прервать наполнение продуктовой корзины введите " + "END" + "\n" +
-                        " Для продолжения программы нажмите Enter");
-                String deprivateCommand = scanner.nextLine();
+                for (int j = 0; j < personsCollection.size(); j++) {
+                    System.out.println(" Если Вы желаете прервать наполнение продуктовой корзины введите " + "END" + "\n" +
+                            " Для продолжения программы нажмите Enter");
+                    String deprivateCommand = scanner.nextLine();
 
-                if (Objects.equals(deprivateCommand.trim(), "END")) {
-                    isEnoughFlag = true;
-                    break;
+                    if (Objects.equals(deprivateCommand.trim(), "END")) {
+                        isEnoughFlag = true;
+                        break;
+                    }
+
+                    Person currentPerson = personsCollection.get(j);
+                    Product currentProduct = productsCollection.get(i);
+                    DiscountProduct currentDiscountProduct = discountProductsCollection.get(k);
+                    currentPerson.setProductsPackage(currentProduct);
+                    currentPerson.setProductsPackage(currentDiscountProduct);
+
+                    if (j == personsCollection.size() && i == productsCollection.size() && !isEnoughFlag) {
+                        isEnoughFlag = true;
+                    }
+
+
                 }
-
-                Person currentPerson = personsCollection.get(j);
-                Product currentProduct = productsCollection.get(i);
-                currentPerson.setProductsPackage(currentProduct);
-
-                if (j == personsCollection.size() && i == productsCollection.size() && !isEnoughFlag) {
-                    isEnoughFlag = true;
-                }
-
-
             }
         }
         for (Person person : personsCollection) {
