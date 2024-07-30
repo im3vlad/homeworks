93222f58c292547a2457b690115a687b2ccc3ced
Vlad <vlad_kopychev@mail.ru>
2024-07-30 15:29:09 +0300

    дз 10 создал классы и интерфейс

---
+++ src/homework10/ByCondition.java
@@ -0,0 +1,5 @@
+package homework10;
+
+public interface ByCondition{
+        boolean isOk(int number);
+}
---
+++ src/homework10/Sequence.java
@@ -0,0 +1,11 @@
+package homework10;
+
+public class Sequence {
+    public static int[] filter(int[] array,ByCondition condition){
+        int[] result = new int[array.length];
+        for(int i=0;i<array.length;i++){
+        }
+        return result;
+    }
+
+}
